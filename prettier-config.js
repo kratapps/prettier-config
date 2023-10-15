7888731fbdcf7fc8f2fed8aa3fe543a1decab2b5
kratoon <ondrej@kratochvil.dev>
2023-10-15 12:02:16 +0200

    Sort imports.

--- prettier-config.js
+++ prettier-config.js
@@ -1,52 +1,63 @@
 module.exports = {
-    trailingComma: "none",
-    overrides: [
+    "trailingComma": "none",
+    "importOrder": [
+        "^lwc$",
+        "^@salesforce(.*)$",
+        "^lightning/(.*)$",
+        "^@(.*)/(.*)$",
+        "^c/(.*)",
+        "^[./]"
+    ],
+    "importOrderParserPlugins": ["decorators"],
+    "importOrderSeparation": true,
+    "importOrderSortSpecifiers": true,
+    "overrides": [
         {
-            files: "**/*.ts",
-            options: {
-                printWidth: 140,
-                tabWidth: 4,
-                singleQuote: true
+            "files": "**/*.ts",
+            "options": {
+                "printWidth": 140,
+                "tabWidth": 4,
+                "singleQuote": true
             }
         },
         {
-            files: "**/*.js",
-            options: {
-                printWidth: 140,
-                tabWidth: 4,
-                singleQuote: true
+            "files": "**/*.js",
+            "options": {
+                "printWidth": 140,
+                "tabWidth": 4,
+                "singleQuote": true
             }
         },
         {
-            files: "**/*.json",
-            options: {
-                printWidth: 140,
-                tabWidth: 2,
-                singleQuote: true
+            "files": "**/*.json",
+            "options": {
+                "printWidth": 140,
+                "tabWidth": 2,
+                "singleQuote": true
             }
         },
         {
-            files: "**/lwc/**/*.html",
-            options: {
-                parser: "lwc",
-                printWidth: 140,
-                tabWidth: 4
+            "files": "**/lwc/**/*.html",
+            "options": {
+                "parser": "lwc",
+                "printWidth": 140,
+                "tabWidth": 4
             }
         },
         {
-            files: "**/*.{cls,trigger,apex}",
-            options: {
-                apexInsertFinalNewline: false,
-                printWidth: 140,
-                tabWidth: 4
+            "files": "**/*.{cls,trigger,apex}",
+            "options": {
+                "apexInsertFinalNewline": false,
+                "printWidth": 140,
+                "tabWidth": 4
             }
         },
         {
-            files: "*.{cmp,page,component}",
-            options: {
-                parser: "html",
-                printWidth: 140,
-                tabWidth: 4
+            "files": "*.{cmp,page,component}",
+            "options": {
+                "parser": "html",
+                "printWidth": 140,
+                "tabWidth": 4
             }
         },
         {
@@ -57,3 +68,4 @@ module.exports = {
         }
     ]
 }
+
